4e9166b7bf9f443bd16a652f2c68f9410cc837cb
Shashwat Shivam <shivamshaswat@gmail.com>
2019-06-20 20:39:56 +0530

    Added rules File

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Fl123utter Demo Home Page'),
     );
   }
 }
